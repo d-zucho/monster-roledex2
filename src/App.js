ecfe91a64bfe4aa7a4af052edb3165a966419b1c
D_Zucho <danny.nico.dev@outlook.com>
2022-04-20 15:27:36 -0700

    start connection with dummy API to test fetch connection

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      // if no original data exists, set the object to empty
+      monsters: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((resp) => resp.json())
+      .then((users) =>
+        this.setState(() => {
+          return {
+            monsters: users,
+          }
+        })
+      )
+    // .then((users) => (users.ma))
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
