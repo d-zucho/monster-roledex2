a2685359d40e3a37355e569a78959c54f909c0a4
DannyZucho <danny.nico.dev@outlook.com>
2022-04-21 06:07:56 -0700

    add filter functionality

--- src/App.js
+++ src/App.js
@@ -7,28 +7,56 @@ class App extends Component {
     this.state = {
       // if no original data exists, set the object to empty
       monsters: [],
+      filterText: '',
     }
+    console.log('constructor')
   }
 
   componentDidMount() {
+    console.log('componentDidMount')
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((resp) => resp.json())
       .then((users) =>
         this.setState(() => {
           return {
+            ...this.state,
             monsters: users,
           }
         })
       )
-    // .then((users) => (users.ma))
+  }
+
+  addFilterText = (e) => {
+    this.setState(() => {
+      return {
+        ...this.state,
+        filterText: e.target.value,
+      }
+    })
   }
 
   render() {
+    console.log('render')
+    const { monsters, filterText } = this.state
+
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(filterText.toLowerCase())
+    )
+
     return (
       <div className='App'>
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <input
+          type='search'
+          className='search-box'
+          placeholder='Search Monster'
+          onChange={this.addFilterText}
+        />
+        {
+          // display filtered monsters
+          filteredMonsters.map((monster) => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))
+        }
       </div>
     )
   }
