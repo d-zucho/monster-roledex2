518f8b810c688cd4416f22964be79fc06a769a81
D_Zucho <danny.nico.dev@outlook.com>
2022-04-29 06:17:59 -0700

    refactor to functional component

--- src/components/card-list/CardList.component.js
+++ src/components/card-list/CardList.component.js
@@ -2,18 +2,14 @@ import { Component } from 'react'
 import MonsterCard from '../monster-card/MonsterCard'
 import './cardList.styles.css'
 
-class CardList extends Component {
-  render() {
-    const { monsters } = this.props
-    console.log(monsters)
-    return (
-      <div className='monsterCards-section'>
-        {monsters.map((monster) => (
-          <MonsterCard key={monster.id} monster={monster} />
-        ))}
-      </div>
-    )
-  }
+const CardList = ({ monsters }) => {
+  return (
+    <div className='monsterCards-section'>
+      {monsters.map((monster) => (
+        <MonsterCard key={monster.id} monster={monster} />
+      ))}
+    </div>
+  )
 }
 
 export default CardList
--- src/components/filter-textbox/FilterTextBox.js
+++ src/components/filter-textbox/FilterTextBox.js
@@ -1,19 +1,15 @@
 import { Component } from 'react'
 import './fileterTextbox.styles.css'
 
-class FilterTextBox extends Component {
-  render() {
-    console.log('from filterTextBox')
-    console.log(this.props)
-    return (
-      <input
-        type='search'
-        className='filterText-box'
-        placeholder='Search Monster'
-        onChange={this.props.onChangeHandler}
-      />
-    )
-  }
+const FilterTextBox = ({ onChangeHandler }) => {
+  return (
+    <input
+      type='search'
+      className='filterText-box'
+      placeholder='Search Monster'
+      onChange={onChangeHandler}
+    />
+  )
 }
 
 export default FilterTextBox
